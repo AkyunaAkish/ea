007d489f3a6c230e323c16f1f0358de8e5554ce9
AkyunaAkish <akyunaakish@gmail.com>
2016-08-19 12:48:35 -0600

    initial styling for auth forms done

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,10 +1,55 @@
 import React, { Component } from 'react'
+import {
+  Paper,
+  TextField,
+  Card,
+  CardActions,
+  CardHeader,
+  CardMedia,
+  CardTitle,
+  CardText,
+  FlatButton,
+  RaisedButton
+} from 'material-ui'
 
 class SignIn extends Component {
   render() {
+    const textFieldStyle = {
+      width: '70%'
+    }
+
+    const raisedButtonStyle = {
+      margin: 12
+    }
+
     return (
-      <div>
-        <h1>SignIn</h1>
+      <div className='authContainer'>
+        <Card className='authCard'>
+          <CardMedia
+            overlay={<CardTitle title='SIGN IN' />}
+            >
+          </CardMedia>
+        </Card>
+        <Paper className='authForm' zDepth={5}>
+          <TextField
+            className='authTextField'
+            style={textFieldStyle}
+            floatingLabelText='Email'
+            hintText='Email'
+            errorText='This field is required'
+            />
+          <TextField
+            className='authTextField'
+            style={textFieldStyle}
+            floatingLabelText='Password'
+            hintText='Password'
+            errorText='This field is required'
+            />
+          <RaisedButton
+            label='SIGN IN'
+            className='authButton'
+            style={raisedButtonStyle} />
+        </Paper>
       </div>
     )
   }
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,10 +1,69 @@
 import React, { Component } from 'react'
+import {
+  Paper,
+  TextField,
+  Card,
+  CardActions,
+  CardHeader,
+  CardMedia,
+  CardTitle,
+  CardText,
+  FlatButton,
+  RaisedButton
+} from 'material-ui'
 
 class SignUp extends Component {
   render() {
+    const textFieldStyle = {
+      width: '70%'
+    }
+
+    const raisedButtonStyle = {
+      margin: 12
+    }
+
     return (
-      <div>
-        <h1>SignUp</h1>
+      <div className='authContainer'>
+        <Card className='authCard' key={0}>
+          <CardMedia
+            overlay={<CardTitle title='SIGN UP' />}
+            >
+          </CardMedia>
+        </Card>
+        <Paper className='authForm' zDepth={5}>
+          <TextField
+            className='authTextField'
+            style={textFieldStyle}
+            floatingLabelText='User Name'
+            hintText='User Name'
+            errorText='This field is required'
+            />
+          <TextField
+            className='authTextField'
+            style={textFieldStyle}
+            floatingLabelText='Email'
+            hintText='Email'
+            errorText='This field is required'
+            />
+          <TextField
+            className='authTextField'
+            style={textFieldStyle}
+            floatingLabelText='Password'
+            hintText='Password'
+            errorText='This field is required'
+            />
+          <TextField
+            className='authTextField'
+            style={textFieldStyle}
+            floatingLabelText='Re-Type Password'
+            hintText='Re-Type Password'
+            errorText='This field is required'
+            />
+          <RaisedButton
+            label='SIGN UP'
+            className='authButton'
+            style={raisedButtonStyle} />
+        </Paper>
       </div>
     )
   }
